652c21eafee94398baa76e9a6edfcc56d42d3979
Mohsin Abbas <maarjojo99@gmail.com>
2025-11-12 23:31:14 +0500

    new componet add to this project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import HomePage from "./components/WelcomeScreen";
+import WorkScreen from "./components/workScreen";
 
 function App() {
 
   return (
+    <>
     <HomePage/>
+    <WorkScreen/>    
+    </>
+    
   );
 }
 
